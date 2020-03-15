bf246cb2b4f1e1bb59c4363ab9028d4ffc612d37
Wei Chen (SW-GPU) <weich@nvidia.com>
2020-03-15 00:23:19 -0600

    [fix bug]

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     num_test_batch = int(0.3 * len(dataloader)) // param.batch_size
 
     print("Start learning:")
-    epoch_output = []
+    epoch_predict = []
     epoch_gt = []
     with tf.Session(graph=g.graph) as sess:
         sess.run(tf.global_variables_initializer())        
@@ -50,11 +50,11 @@ if __name__ == '__main__':
                 train_writer.add_summary(summary, b + num_train_batch * epoch)
 
             # testing
-            batch_output = []
+            batch_predict = []
             batch_gt = []
             for b in tqdm(range(num_test_batch)):
                 x_closeness, x_period, x_trend, y_batch = next(test_batch_generator)                
-                loss_v, output, summary = sess.run([g.loss, g.output, g.merged],
+                loss_v, predict, summary = sess.run([g.loss, g.x_res, g.merged],
                                             feed_dict={g.c_inp: x_closeness,
                                                        g.p_inp: x_period,
                                                        g.t_inp: x_trend,
@@ -63,11 +63,12 @@ if __name__ == '__main__':
                 val_writer.add_summary(summary, b + num_test_batch * epoch)
                 
                 # prediction output [batch_size, H, W, 1] 
-                denormalized_output = dataloader.inverse_transform(output)
-                batch_output.append(denormalized_output)
-                batch_gt.append(y_batch)
+                denormalized_predict = dataloader.inverse_transform(predict)
+                batch_predict.append(denormalized_predict) 
+                denormalized_y_batch = dataloader.inverse_transform(y_batch)
+                batch_gt.append(denormalized_y_batch)
 
-            batch_output = np.stack(batch_output, axis=0)
+            batch_predict = np.stack(batch_predict, axis=0)
             batch_gt = np.stack(batch_gt, axis=0)
             if num_test_batch != 0:
                 loss_val /= num_test_batch
@@ -75,13 +76,13 @@ if __name__ == '__main__':
             print("loss: {:.3f}, val_loss: {:.3f}".format(loss_train, loss_val))  
             # save the model after every epoch         
             g.saver.save(sess, "/tmp/model")
-            epoch_output.append(batch_output)
+            epoch_predict.append(batch_predict)
             epoch_gt.append(batch_gt)
 
-    epoch_output = np.stack(epoch_output, axis=0)
+    epoch_predict = np.stack(epoch_predict, axis=0)
     epoch_gt = np.stack(epoch_gt, axis=0)
-    np.save("outputs", epoch_output)
-    np.save("gts", epoch_gt)
+    np.save("predict", epoch_predict)
+    np.save("gt", epoch_gt)
     train_writer.close()
     val_writer.close()
     print("Run 'tensorboard --logdir=./logdir' to checkout tensorboard logs.")
